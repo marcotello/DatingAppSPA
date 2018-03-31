e471b65f5ce2607cbb43bc11700439a1ea23b56c
Marco Tello <muchaweb@gmail.com>
2018-03-30 21:14:51 -0600

    adding form validations to the register form

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
 import { AlertifyService } from '../_services/alertify.service';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -10,18 +11,45 @@ import { AlertifyService } from '../_services/alertify.service';
 export class RegisterComponent implements OnInit {
   model: any = {};
   @Output() cancelRegister = new EventEmitter();
+  registerForm: FormGroup;
 
-  constructor(private authService: AuthService, private alertify: AlertifyService) { }
+  constructor(private authService: AuthService, private alertify: AlertifyService, private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.createRegisterForm();
+  }
+
+  createRegisterForm() {
+    this.registerForm = this.formBuilder.group({
+      gender: ['male'],
+      username: ['', Validators.required],
+      knownAs: ['', Validators.required],
+      dateOfBirth: [null, Validators.required],
+      city: ['', Validators.required],
+      country: ['', Validators.required],
+      password: ['', [
+          Validators.required,
+          Validators.minLength(4),
+          Validators.maxLength(16)
+        ]
+      ],
+      confirmPassword: ['', Validators.required]
+    }, {
+      validator: this.passwordMatchValidator
+    });
+  }
+
+  passwordMatchValidator(formGroup: FormGroup) {
+    return formGroup.get('password').value === formGroup.get('confirmPassword').value ? null : {'mismatch' : true};
   }
 
   register() {
-    this.authService.register(this.model).subscribe(() => {
+    /*this.authService.register(this.model).subscribe(() => {
       this.alertify.success('Registration Successful');
     }, error => {
       this.alertify.error(error);
-    });
+    });*/
+    console.log(this.registerForm.value);
   }
 
   cancel() {
